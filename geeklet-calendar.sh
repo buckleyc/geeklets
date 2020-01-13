f24a53dc9e947540e4d6a7680c6ed747c49df62a
Buckley Collum <buckleywhat@gmail.com>
2020-01-13 09:58:06 -0800

    Update geeklet-calendar.sh

--- geeklet-calendar.sh
+++ geeklet-calendar.sh
@@ -2,7 +2,8 @@
 # Calendar - provides info about calendar events
 # requires gcalcli (pip install gcalcli)
 
-if command -v gcalcli >/dev/null 2>&1 ; then
-  # hostname
-  gcalcli --lineart fancy agenda
+if [ -x /usr/local/bin/gcalcli ] ; then # command -v gcalcli >/dev/null 2>&1 ; then
+  # get calendar events from Google Calendar
+  echo "CALENDAR take 2"
+  /usr/local/bin/gcalcli --lineart fancy agenda
 fi
